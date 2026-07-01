311c2e6305871a843f4d5c9ca75984be4e72dca0
Toad64 <af4toad@gmail.com>
2026-06-30 22:13:26 -0400

    a lotta shite

--- gamemodes/QUAKE/gamemode/hud.lua
+++ gamemodes/QUAKE/gamemode/hud.lua
@@ -3,7 +3,7 @@ local COLOR_RED    = Color(255, 0, 0, 255)
 
 function HUD()
     local client = LocalPlayer()
-    local width = 500
+    local width = 750
     local activeWeapon = client:GetActiveWeapon()
     local ammotext = "0"
 
@@ -26,7 +26,7 @@ function HUD()
         ammocolor = COLOR_RED
     end
 
-    -- draw.RoundedBox(10, (ScrW()/2)-(width/2), ScrH() - 100, width, 90, Color(50, 50, 50, 200))
+    draw.RoundedBox(10, (ScrW()/2)-(width/2), ScrH() - 100, width, 90, Color(50, 50, 50, 200))
     draw.SimpleText(""..client:Health(), "QuakeFontLarge", (ScrW()/2)-35, ScrH() - 55, healthcolor, 1, 0)
     draw.SimpleText(""..client:Armor(), "QuakeFontLarge", (ScrW()/2)+220, ScrH() - 55, COLOR_QUAKE, 1, 0)
     draw.SimpleText(ammotext, "QuakeFontLarge", (ScrW()/2)-220, ScrH() - 55, ammocolor, 1, 0)
@@ -48,6 +48,7 @@ end
 hook.Add("HUDShouldDraw", "HideDefaultHud", HideHud)
 
 -- vgui for player model
+
 PMPanel = vgui.Create("DPanel")
 PMPanel:SetPos((ScrW() / 2) + 35, ScrH() - 100)
 PMPanel:SetSize(90, 90)
@@ -55,11 +56,11 @@ PMPanel:SetBackgroundColor(Color(0, 255, 0, 255))
 
 local mdl = vgui.Create("DModelPanel", PMPanel)
 mdl:SetSize(PMPanel:GetSize())
-mdl:SetModel("models/player/quakeguy.mdl")
+mdl:SetModel("models/akuld/qeranger/qeranger.mdl")
 
-mdl:SetCamPos(Vector(12, 0, 64))   -- Camera position (X, Y, Z)
-mdl:SetLookAt(Vector(0, 0, 64))    -- Where the camera points (X, Y, Z)
-mdl:SetFOV(90)                     -- FOV (Lower = closer)
+mdl:SetCamPos(Vector(20, 0.5, 69))   -- Camera position (X, Y, Z)
+mdl:SetLookAt(Vector(0, 0, 65))    -- Where the camera points (X, Y, Z)
+mdl:SetFOV(45)                     -- FOV (Lower = closer)
 
 local ent = mdl.Entity
 if IsValid(ent) then
@@ -72,12 +73,12 @@ function mdl:LayoutEntity(ent) return end
 ArmPanel = vgui.Create("DPanel")
 ArmPanel:SetPos((ScrW() / 2) + 280, ScrH() - 100)
 ArmPanel:SetSize(90, 90)
-ArmPanel:SetBackgroundColor(Color(0, 255, 0, 0))
+ArmPanel:SetBackgroundColor(Color(0, 255, 0))
 
 local armor = vgui.Create("DModelPanel", ArmPanel)
 armor:SetSize(ArmPanel:GetSize())
 armor:SetModel("models/items/quake1/armor1.mdl")
-armor:SetVisible(false)
+armor:SetVisible(true)
 
 armor:SetCamPos(Vector(-90, 0, 50))   -- Camera position (X, Y, Z)
 armor:SetLookAt(Vector(64, 0, 0))    -- Where the camera points (X, Y, Z)
@@ -87,29 +88,49 @@ function armor:Think()
     local ply = LocalPlayer()
     if not IsValid(ply) then return end
 
-    local currentArmor = ply:Armor()
-
-    -- If armor is 0, turn the panel off completely
-    if currentArmor <= 0 then
-        if self:IsVisible() then self:SetVisible(false) end
-        return
-    else
-        if not self:IsVisible() then self:SetVisible(true) end
+    -- 1. Get the raw value
+    local activeTier = ply:GetNWInt("QuakeArmorTier", 0)
+    
+    -- 2. Defensive check: If armor > 0 but Tier is 0, something is broken. 
+    -- Force a refresh or default to Tier 1 if the player has armor but no tier is set.
+    if ply:Armor() > 0 and activeTier == 0 then
+        activeTier = 1 
     end
 
-    -- Check what tier the player has active
-    local activeTier = ply:GetNWInt("QuakeArmorTier", 1)
-    local targetModel = "models/items/quake1/armor1.mdl" -- Default Green
+    -- 3. Calculate model path safely
+    local models = {
+        [1] = "models/items/quake1/armor1.mdl",
+        [2] = "models/items/quake1/armor2.mdl",
+        [3] = "models/items/quake1/armor3.mdl"
+    }
+    local targetModel = models[activeTier] or models[1]
 
-    if activeTier == 2 then
-        targetModel = "models/items/quake1/armor2.mdl" -- Yellow
-    elseif activeTier == 3 then
-        targetModel = "models/items/quake1/armor3.mdl" -- Red
-    end
-
-    -- Only update the model if it actually changed to save performance
+    -- 4. Only update if the entity is actually valid and needs a change
     if self.CurrentModel ~= targetModel then
         self:SetModel(targetModel)
         self.CurrentModel = targetModel
+        
+        -- Reset the entity view if it exists
+        if IsValid(self.Entity) then
+            self.Entity:SetAngles(Angle(0, 45, 0)) -- Set a standard rotation
+        end
     end
+    
+    -- 5. Visibility toggle
+    self:SetVisible(ply:Armor() > 0)
 end
+
+-- vgui for ammo
+AmmoPanel = vgui.Create("DPanel")
+AmmoPanel:SetPos((ScrW() / 2) - 370, ScrH() - 100)
+AmmoPanel:SetSize(90, 90)
+AmmoPanel:SetBackgroundColor(Color(0, 255, 0))
+
+local ammo = vgui.Create("DModelPanel", AmmoPanel)
+ammo:SetSize(ArmPanel:GetSize())
+ammo:SetModel("models/items/quake1/armor1.mdl")
+ammo:SetVisible(true)
+
+ammo:SetCamPos(Vector(-90, 0, 50))   -- Camera position (X, Y, Z)
+ammo:SetLookAt(Vector(64, 0, 0))    -- Where the camera points (X, Y, Z)
+ammo:SetFOV(25)  
--- gamemodes/QUAKE/gamemode/init.lua
+++ gamemodes/QUAKE/gamemode/init.lua
@@ -6,9 +6,14 @@ include("shared.lua")
 
 function GM:PlayerSpawn(ply)
     self.BaseClass.PlayerSpawn( self, ply )
-    ply:SetModel( "models/player/quakeguy.mdl" )
-    ply:SetupHands()
+
+    if GetGlobalInt("meru_active") == 1 then
+        ply:SetModel("models/alvaroports/samzan/merusuccubuspm.mdl")
+    else
+        ply:SetModel("models/akuld/qeranger/qeranger.mdl")
+    end
     
+    ply:SetupHands()
 
     if game.GetMap() != "q1_training" then
         ply:Give("weapon_q1_axe")
